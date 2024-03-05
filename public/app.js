db5d19e6bf93c2c5d13002f76192991e38236847
Abdinasir <samiradynassir@gmail.com>
2024-03-05 12:22:51 +0300

    added a show more button

--- public/app.js
+++ public/app.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const projectsSection = document.getElementById('projects');
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        projectsSection.classList.add('visible');
-        observer.unobserve(projectsSection);
-      }
-    });
-  }, { threshold: 0.1 });
-
-  observer.observe(projectsSection);
-});
 // Toggle mobile menu
 function toggleMobileMenu() {
     const mobileMenu = document.getElementById("mobileMenu");
@@ -29,11 +16,3 @@ function showMoreProjects() {
     showMoreButton.textContent = showMoreButton.textContent === "Show more" ? "Show less" : "Show more";
 }
 
-// Toggle service details
-function toggleServiceDetails(serviceId) {
-    const service = document.getElementById(serviceId);
-    service.classList.toggle("hidden");
-
-    const arrow = document.getElementById(`arrow-${serviceId}`);
-    arrow.classList.toggle("rotate-180");
-}

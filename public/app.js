9abade1f44c2356298880d37b3775ce6c820513f
Abdinasir <samiradynassir@gmail.com>
2024-03-06 12:34:04 +0300

    chnaged my nav tech to skills

--- public/app.js
+++ public/app.js
@@ -1,3 +1,30 @@
+// smooth scroll for navigation 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetedSection = document.querySelector(this.getAttribute('href'));
+      const navHeight = document.querySelector('nav').offsetHeight;
+      let extraPadding = 0;
+
+      if (window.innerWidth >= 768) {
+          extraPadding = 50;
+      }
+
+      window.scrollTo({
+          top: targetedSection.offsetTop - navHeight - extraPadding,
+          behavior: 'smooth'
+
+      });
+
+      // reset url to root path
+      if (window.location.hash) {
+          window.location.replace(window.location.pathname)
+      }
+
+  })
+})
+
 // Toggle mobile menu
 function toggleMobileMenu() {
     const mobileMenu = document.getElementById("mobileMenu");
